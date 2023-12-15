c78ca77a2d8194f1f50fe69eb58bc501d1c40b0b
Francesco <francesco.pasquinoni@hotmail.it>
2023-12-15 16:05:38 +0100

    creazione variabili + prompt. Validazione a console

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 console.log("JS OK");
 
+//! Step 1 / 2
+
+const firstname = prompt("Qual è il tuo nome?" , "Francesco");
+const surname = prompt("Il tuo cognome?" , "Pasquinoni");
+const name = prompt("Qual è il tuo colore preferito?" , "blu");
+let random_val = ("23");
+
+/* Validazione */
+console.log(firstname, surname, name, random_val);
+
+
