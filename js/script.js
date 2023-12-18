023294e4873ef947abac78f6c20d8a57ecafe591
Francesco <francesco.pasquinoni@hotmail.it>
2023-12-18 13:12:54 +0100

    + pills

--- js/script.js
+++ js/script.js
@@ -30,3 +30,38 @@ console.log(span);
 span.innerText += password
 
 console.log(span);
+
+//! PILLS
+
+// PILL 1:  Dichiara una variabile chiamata greet e assegnale il valore "Ciao, Mondo!". Stampa il valore della variabile in console.
+
+/*
+const greet = ("Ciao, Mondo!")
+
+console.log(greet)
+*/
+
+/* PILL 2: Dichiara una variabile chiamata greet assegnandole il valore "Ciao Mondo" e stampa il valore in console. 
+Poi modifica il valore della variabile greet in modo da ottenere la stringa "Ciao, Mondo! Come stai?". 
+Stampa il valore della variabile in console. 
+Bonus: Prova a eseguire questo esercizio sia con la concatenazione di stringhe sia con i template literals.
+*/
+
+/*
+let greet = ("Ciao Mondo")
+
+console.log(greet)
+
+greet = greet + " Come stai?"
+
+console.log(greet)
+*/
+
+//PILLS 3 Crea una variabile comparison e assegnale il risultato del confronto tra due numeri per verificare se il primo numero è maggiore del secondo. Stampa sulla console il valore di comparison
+/*
+const firstNumber = 1;
+const secondNumber = 2;
+
+const comparison = firstNumber > secondNumber;
+console.log(comparison)
+*/
